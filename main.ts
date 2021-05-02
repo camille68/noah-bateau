6a4c141365648d914bf840f881cf504eac9073c7
camille68 <camille68@gmail.com>
2021-05-02 21:02:13 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
-kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
 kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
+kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
 basic.forever(function () {
 	
 })
