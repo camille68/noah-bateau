0ea11ccb6298c03f576c9ad9bc605e30e82a2fa4
camille68 <camille68@gmail.com>
2021-05-04 20:40:56 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,26 @@
 radio.onReceivedValue(function (name, value) {
-    if (0 == 0) {
+    if (name == "p_1" && value == 0) {
+        basic.showNumber(1)
         kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
         kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
-    } else if (0 >= 0) {
-        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Forward, 0)
-        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Reverse, 0)
+    } else if (name == "p_1" && value > 0) {
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Forward, value)
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Reverse, value)
+    } else if (name == "p_1" && value < 0) {
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, value)
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, value)
     } else {
-        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, 0)
-        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 0)
-    }
-    if (0 >= 0) {
     	
+    }
+    if (name == "p_2" && value == 0) {
+        kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
+        kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
+    } else if (name == "p_2" && value > 0) {
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, value)
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Forward, value)
+    } else if (name == "p_2" && value < 0) {
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, value)
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Reverse, value)
     } else {
     	
     }
