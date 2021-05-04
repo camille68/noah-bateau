4ee88880ffeb4c1d83935ecb422093162ce8ed91
camille68 <camille68@gmail.com>
2021-05-04 12:43:13 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,20 @@
+radio.onReceivedValue(function (name, value) {
+    if (0 == 0) {
+        kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
+        kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
+    } else if (0 >= 0) {
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Forward, 0)
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Reverse, 0)
+    } else {
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, 0)
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 0)
+    }
+    if (0 >= 0) {
+    	
+    } else {
+    	
+    }
+})
+radio.setGroup(197)
 kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
 kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
-basic.forever(function () {
-	
-})
